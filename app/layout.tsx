e368a057f2ceb892dd0b77ffefdecd281a5129b6
Shahriar Hasan Siam <siamshahriarhasan@gmail.com>
2025-08-27 22:43:44 +0600

    Rename project from "my-v0-project" to "shahriar-hasan-siam" in package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { GeistMono } from "geist/font/mono"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Shahriar Hasan Siam",
+  description: "MERN Stack Portfolio Website of Shahriar Hasan Siam"
 }
 
 export default function RootLayout({
