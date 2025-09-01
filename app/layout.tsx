4f7307bda1917420e59113dd269eda0b95e72bd5
Shahriar Hasan Siam <siamshahriarhasan@gmail.com>
2025-09-02 01:27:11 +0600

    monetize: Adsterra

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>{children}</body>
+      <head>
+        {/* <meta name="monetag" content="b1e62672f13ca681ad70a7ee51c2fa67" /> */}
+        <script type='text/javascript' src='//pl27556517.revenuecpmgate.com/c9/ed/35/c9ed35deffd5c9afc998400ca76ef1cb.js'></script>
+      </head>
+      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
